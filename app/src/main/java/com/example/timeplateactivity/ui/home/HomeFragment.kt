6403ba164b871ecc7c2c3b9bae0b65062617dd55
KostinMikhail <79811272328@ya.ru>
2022-04-28 22:04:26 +0300

    round and pause stop and resume working

--- app/src/main/java/com/example/timeplateactivity/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/timeplateactivity/ui/home/HomeFragment.kt
@@ -12,6 +12,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -23,16 +24,38 @@ import java.text.MessageFormat.format
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.properties.Delegates
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
    private var _binding: FragmentHomeBinding? = null
    private val binding get() = _binding!!
-   var setRoundsAmount: Int = 1
+
    var timer: CountDownTimer? = null
    var timerRunning: Boolean = false
+   var timerOnRest: Boolean = false
+
+   var roundTime: Long = 4000
+   var restTime: Long = 8000
+   val tick: Long = 1000
+   var setRoundsAmount1: Int = 1
+
+   var currentRoundTime: Long? = null
+   var currentRestTime: Long? = null
+   var whatRound: Int? = null
+
+//   var timerStatus: Int = 0 //0 is for not in work, 1 is for roundTime, 2 is for restTime
 
 
+    fun resumeTimer(){
+//        timerStatus = 1
+        roundTimer(currentRoundTime!!, tick, whatRound!!)
+
+    }
+
+    fun resumeRestTimer(){
+        restTimer(currentRestTime!!, tick)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,20 +74,67 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         }
         root.setBackgroundResource(R.drawable.blue_gradient)
 
-        binding.btnStart.setOnClickListener{
-        newTimer(4000, 3000, 1000, 1)
-        if (timerRunning) {
-
-        }   else {
-            timerRunning = false
-            binding.btnStart.isEnabled = false
+        fun cancelTimer() {
+//            timerStatus = 0
+            timer?.cancel()
         }
 
+
+
+        binding.btnStart.setOnClickListener{
+        roundTimer(roundTime, tick, setRoundsAmount1)
+        timerRunning = true
+        binding.btnPause.isVisible = true
+//        timerStatus = 1
+
+        if (timerRunning) {
+            binding.btnStart.setOnClickListener{
+                cancelTimer()
+                roundTimer(roundTime,tick, 1)
+                binding.btnPause.isVisible = true
+                binding.btnResume.isVisible = false
+//                timerStatus = 1
+
+            }
+
+
+        }   else {
+
+        }
+
+ /*       if (timerOnStop){
+
+        }   else {
+
+        }*/
+
             val amountOfRounds: TextView = binding.amountOfRounds
-            amountOfRounds.text = "Round  " + setRoundsAmount
+            amountOfRounds.text = "Round  " + setRoundsAmount1
 
                 }
+        binding.btnPause.setOnClickListener{
+            cancelTimer()
+//            timerStatus = 0
+            binding.btnStart.setText("restart")
+            binding.btnResume.isVisible = true
+
+        }
+
+
+
+        binding.btnResume.setOnClickListener{
+            if (timerOnRest){
+                resumeRestTimer()
+            } else {
+            cancelTimer()
+            resumeTimer()
+            }
+        }
+
+
+
         return root
+
 
     }
 
@@ -73,7 +143,9 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         _binding = null
 
     }
-    fun newTimer (roundTime: Long, restTime: Long, tick: Long,  setRoundsAmount: Int ){
+
+
+    fun roundTimer (roundTime: Long, tick: Long,  setRoundsAmount: Int ){
 
         var currentRound = setRoundsAmount
         binding.amountOfRounds.text = "Round  " + currentRound
@@ -81,33 +153,77 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         timer =  object : CountDownTimer(roundTime, tick) {
 
             override fun onTick(roundTime: Long) {
-                val date = Date(roundTime)
+                val round = Date(roundTime)
                 var formatter = SimpleDateFormat("mm:ss")
-                val a = formatter.format(date)
+                val a = formatter.format(round)
+                currentRoundTime = roundTime
+                whatRound = currentRound
+
+
                 binding.textHome.setText("seconds remaining: " + a)
 
-            }
 
+            }
 
             override fun onFinish() {
                 binding.amountOfRounds.text = "Round  " + currentRound
 
                 if (currentRound < 3) {
-                    currentRound ++
-                    newTimer(roundTime, 3000,1000, currentRound )
+                    setRoundsAmount1 ++
+                    restTimer (restTime, tick)
 
                 } else {
 
                     binding.textHome.setText("done!")
                     binding.root.setBackgroundResource(R.drawable.green_gradient)
-                    binding.btnStart.isEnabled = true
-                    currentRound = 1
+//                    binding.btnStart.isEnabled = true
+                    setRoundsAmount1 = 1
                     binding.amountOfRounds.setText("start again")
-
+                    binding.btnPause.isVisible = false
+                    binding.btnResume.isVisible = false
                 }
 
             }
         }.start()
 
     }
+    fun restTimer (restTime: Long, tick: Long){
+
+        var currentRound = setRoundsAmount1
+        binding.amountOfRounds.text = "Rest"
+
+        timer =  object : CountDownTimer(restTime, tick) {
+
+            override fun onTick(restTime: Long) {
+                val rest = Date(restTime)
+                var formatter = SimpleDateFormat("mm:ss")
+                val b = formatter.format(rest)
+                binding.textHome.setText("resting: " + b)
+                currentRestTime = restTime
+                timerOnRest = true
+
+
+            }
+
+            override fun onFinish() {
+                timerOnRest = false
+                roundTimer(roundTime, tick, currentRound )
+
+            }
+        }.start()
+
+    }
+
 }
+/*
+1) сделать кнопку пауза  24.04
+2) кнопку продолжить/стоп 01.05 *так же продолжать с отдыха
+3) создать возможность настраивать раунды   08.05     читать: sqlite, room
+4) подвязать раунды к режимам 12.05
+5) изменить цвет цифр в зависимости от времени, изменить цвет режима 15.05
+6) добавить звук начала/конца раунда  17.05
+7) добавить 5 сек перед началом старта таймера 19.05
+8) добавить счётчик калорий  22.05
+
+9) раскидать всё по разным директориям (clean)
+ */
