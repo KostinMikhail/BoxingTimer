3dcd25332b4b25d6f3f731d0fbe2a3f5d4bc5b3a
KostinMikhail <79811272328@ya.ru>
2022-09-06 23:06:56 +0300

    calcInProgress

--- app/src/main/java/com/example/timeplateactivity/ui/calc/CalcFragment.kt
+++ app/src/main/java/com/example/timeplateactivity/ui/calc/CalcFragment.kt
@@ -2,9 +2,11 @@ package com.example.timeplateactivity.ui.calc
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -13,9 +15,14 @@ import com.example.timeplateactivity.databinding.FragmentCalcBinding
 
 class CalcFragment : Fragment() {
 
+    var isMale: Boolean = true
+    var height: Int? = null
+    var age: Int? = null
+    var weight: Int? = null
+    var activity: Int? = null
+
+
     private var _binding: FragmentCalcBinding? = null
-
-
 
     private val binding get() = _binding!!
 
@@ -29,20 +36,21 @@ class CalcFragment : Fragment() {
             ViewModelProvider(this).get(CalcViewModel::class.java)
 
         _binding = FragmentCalcBinding.inflate(inflater, container, false)
-with (binding){
-//    spinner.getBackground().setColorFilter(resources)
+        with(binding) {
+            sexBtnMale.setOnClickListener {
+                isMale = true
+            }
+            sexBtnFemale.setOnClickListener {
+                isMale = false
+            }
+//            heightSB.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+//                override fun onProgressChanged(heightSB: SeekBar?, )
+//            })
+
+        }
 
 
-}
-
-        //       binding.sexButtonMale.background.setTint(R.color.primary50Transparent)
-
-
-        /*      val textView: TextView = binding.textSlideshow
-              slideshowViewModel.text.observe(viewLifecycleOwner) {
-                  textView.text = it
-              }
-       */       return binding.root
+        return binding.root
 
 
     }
