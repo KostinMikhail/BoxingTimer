970fe2aed2da9db7c79c130c10acacb16bc83edd
KostinMikhail <79811272328@ya.ru>
2022-09-08 02:23:59 +0300

    calcInProgress2

--- app/src/main/java/com/example/timeplateactivity/ui/calc/CalcFragment.kt
+++ app/src/main/java/com/example/timeplateactivity/ui/calc/CalcFragment.kt
@@ -1,13 +1,18 @@
 package com.example.timeplateactivity.ui.calc
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.timeplateactivity.R
@@ -15,37 +20,115 @@ import com.example.timeplateactivity.databinding.FragmentCalcBinding
 
 class CalcFragment : Fragment() {
 
-    var isMale: Boolean = true
-    var height: Int? = null
-    var age: Int? = null
-    var weight: Int? = null
-    var activity: Int? = null
-
 
     private var _binding: FragmentCalcBinding? = null
 
     private val binding get() = _binding!!
 
+    private var calcViewModel: CalcViewModel? = null
+
+    val result = calcViewModel?.calc().toString()
+
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("ResourceAsColor")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
+        val calcViewModel =
             ViewModelProvider(this).get(CalcViewModel::class.java)
 
         _binding = FragmentCalcBinding.inflate(inflater, container, false)
         with(binding) {
-            sexBtnMale.setOnClickListener {
-                isMale = true
+
+            sexBtnMale.setOnCheckedChangeListener { compoundButton, isChecked ->
+                if (isChecked) {
+                    sexBtnMale.setBackgroundResource(@drawable / btn_sex_male_on)
+                } else {
+                    sexBtnMale.setBackgroundResource(@drawable / btn_sex_male)
+                }
+
+                //               sexBtnMale.setBackgroundResource(@color/primary50Transparent)
             }
-            sexBtnFemale.setOnClickListener {
-                isMale = false
+
+
+//            sexBtnMale.setOnClickListener() {
+//
+//                calcViewModel.setMale()
+//            }
+            sexBtnFemale.setOnClickListener() {
+                calcViewModel.setFemale()
             }
-//            heightSB.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-//                override fun onProgressChanged(heightSB: SeekBar?, )
-//            })
+
+            heightSB.min = 140
+            heightSB.max = 220
+
+            heightSB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    calcViewModel.height = progress
+
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+
+            })
+
+            ageSB.min = 14
+            ageSB.max = 60
+
+            ageSB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    calcViewModel.age = progress
+
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+
+            })
+
+            weightSB.min = 45
+            weightSB.max = 120
+
+            weightSB.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    calcViewModel.weight = progress
+
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+
+            })
+
+            btnCalculate.setOnClickListener() {
+
+                test.setText(result)
+            }
+
 
         }
 
--- app/src/main/java/com/example/timeplateactivity/ui/calc/CalcViewModel.kt
+++ app/src/main/java/com/example/timeplateactivity/ui/calc/CalcViewModel.kt
@@ -3,11 +3,43 @@ package com.example.timeplateactivity.ui.calc
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.timeplateactivity.databinding.FragmentCalcBinding
+
 
 class CalcViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "nutriton calculator here"
+    var isMale: Boolean = true
+    var height: Int? = null
+    var age: Int? = null
+    var weight: Int? = null
+    var activity: Int? = null
+    var result: Double? = null
+
+    fun setMale() {
+        isMale = true
     }
-    val text: LiveData<String> = _text
+
+    fun setFemale() {
+        isMale = false
+    }
+
+    fun calc() {
+        if (isMale) {
+            result = weight!! * 10 + 6.25 * height!! - 5 * age!! + 5 * activity!!
+
+        } else {
+            result = weight!! * 10 + 6.25 * height!! - 5 * age!! - 161 * activity!!
+        }
+
+    }
+
+
+//    private val _text = MutableLiveData<String>().apply {
+//        value = "nutriton calculator here"
+//    }
+//
+//
+//    val text: LiveData<String> = _text
+
+
 }
